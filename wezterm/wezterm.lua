e138457fc249e7adac4e23f30be7bcbc1228fc96
Arcadia047 <64649066+Arcadia047@users.noreply.github.com>
2025-05-14 22:40:11 -0700

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- Font configuration
 config.font = wezterm.font {
-  family = 'JetBrains Mono',
-  harfbuzz_features = { 'calt=0' }, -- Disable ligatures
+    family = 'JetBrains Mono',
+    harfbuzz_features = { 'calt=0' }, -- Disable ligatures
 }
 config.font_size = 18.0
 
@@ -20,12 +21,19 @@ config.scrollback_lines = 2000
 
 -- Key bindings
 config.keys = {
-  -- Ctrl+T to create a new tab with the current working directory
-  {
-    key = 't',
-    mods = 'CTRL',
-    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-  },
+    -- Ctrl+T to create a new tab with the current working directory
+    {
+        key = 't',
+        mods = 'CTRL',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+    },
+    {
+        key = 'k',
+        mods = 'CMD',
+        action = act.Multiple {
+            act.ClearScrollback 'ScrollbackAndViewport'
+        },
+    }
 }
 
 config.color_scheme = 'nord'
